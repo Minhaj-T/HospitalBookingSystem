82c9c23791686344375fb900982c8a9b6998a8e4
minhaj <minhajt.mh@gmail.com>
2022-07-23 21:39:54 +0530

    compleated from admin side charts and transaction table

--- frontend/src/pages/Admin/admindashboard/adminDashboard.js
+++ frontend/src/pages/Admin/admindashboard/adminDashboard.js
@@ -5,6 +5,9 @@ import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchUsers } from '../../../features/admin/auth/adminauthSlice';
 import { allSpecialties } from '../../../features/admin/auth/adminauthSlice';
+import Featured from '../../../components/Admin/featured/Featured';
+import Chart from '../../../components/Admin/Chart/Chart';
+import Table from '../../../components/Admin/Table/Table';
 
 function AdminDashboard() {
   const dispatch = useDispatch();
@@ -23,6 +26,14 @@ function AdminDashboard() {
         <Widgets type="earning" />
         <Widgets type="balance" />
       </div>
+      <div className="charts">
+          <Featured />
+          <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
+      </div>
+      <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <Table />
+        </div>v
     </>
   );
 }
